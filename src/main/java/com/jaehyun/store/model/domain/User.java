89be091d8cfc68913f4d1a53bb28ff51ce557308
Anjaehyun <lvet0330@gmail.com>
2023-05-18 19:19:16 +0900

    #11 jwt token 을 통한 회원가입 및 로그인 구현 초기
    의존성 추가 
        implementation 'org.springframework.boot:spring-boot-starter-security'
        implementation 'io.jsonwebtoken:jjwt:0.9.1'
    
    yml에서 secretIKey 가져옴 (원래는 공개하면 안됨)
        //yml 파일에서 가져온 비밀번호
        @Value("{spring.jwt.secret}")
        private String secretKey;
    
        jwt:
          secret: anjaehyunUsingForTest #원래는 공개하면 안됨.

--- src/main/java/com/jaehyun/store/model/domain/User.java
+++ src/main/java/com/jaehyun/store/model/domain/User.java
@@ -1,22 +1,75 @@
 package com.jaehyun.store.model.domain;
 
 import com.jaehyun.store.model.BaseEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.ColumnDefault;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
-public class User extends BaseEntity {
+public class User extends BaseEntity implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId; //유저 id
 
-    private String userName; //유저 이름
-    private String userPassword;//유저 비밀번호
-
+//    private String userName; //유저 이름
     @Column(unique = true)
     private String userPhoneNum;//유저 휴대폰 번호 겸 아이디
+    private String userPassword;//유저 비밀번호
 
-    @ColumnDefault("false")
-    private boolean auth; //권한
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Builder.Default
+    private List<String> roles = new ArrayList<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.roles.stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 }
