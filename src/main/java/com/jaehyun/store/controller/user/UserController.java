a708856acfe5095250e378e6cdf04d4f510b8325
Anjaehyun <lvet0330@gmail.com>
2023-05-18 19:19:54 +0900

    #12 jwt 토큰 사용하여 로그인 구현
    정상적으로 데이터 삽입 완료
    하지만
    2023-05-18 19:13:50.110 ERROR 4876 --- [nio-8080-exec-5] o.a.c.c.C.[.[.[/].[dispatcherServlet]    : Servlet.service() for servlet [dispatcherServlet] in context with path [] threw exception [Request processing failed; nested exception is java.lang.IllegalArgumentException: There is no PasswordEncoder mapped for the id "null"] with root cause
    오류 발생으로 인한 1차 커밋. 수정 예정

---
+++ src/main/java/com/jaehyun/store/controller/user/UserController.java
@@ -0,0 +1,43 @@
+package com.jaehyun.store.controller.user;
+
+import com.jaehyun.store.config.JwtTokenProvider;
+import com.jaehyun.store.model.repository.UserRepository;
+import com.jaehyun.store.model.domain.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
+
+@RequiredArgsConstructor
+@RestController
+public class UserController {
+
+    private final PasswordEncoder passwordEncoder;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final UserRepository userRepository;
+
+    // 회원가입
+    @PostMapping("/join")
+    public Long join(@RequestBody Map<String, String> user) {
+        return userRepository.save(User.builder()
+                .userPhoneNum(user.get("userPhoneNum"))
+                .userPassword(passwordEncoder.encode(user.get("userPassword")))
+                .roles(Collections.singletonList("USER")) // 최초 가입시 USER 로 설정
+                .build()).getUserId();
+    }
+
+    // 로그인
+    @PostMapping("/login")
+    public String login(@RequestBody Map<String, String> user) {
+        User member = userRepository.findByUserPhoneNum(user.get("userPhoneNum"))
+                .orElseThrow(() -> new IllegalArgumentException("가입되지 않은 userPhoneNum 입니다."));
+        if (!passwordEncoder.matches(user.get("userPassword"), member.getPassword())) {
+            throw new IllegalArgumentException("잘못된 비밀번호입니다.");
+        }
+        return jwtTokenProvider.createToken(member.getUserPhoneNum(), member.getRoles());
+    }
+}
--- src/main/java/com/jaehyun/store/model/repository/UserRepository.java
+++ src/main/java/com/jaehyun/store/model/repository/UserRepository.java
@@ -1,4 +1,10 @@
 package com.jaehyun.store.model.repository;
 
-public interface UserRepository {
+import com.jaehyun.store.model.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUserPhoneNum(String userPhoneNum);
 }
