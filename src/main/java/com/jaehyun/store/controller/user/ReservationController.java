681458f70d45b72937e859dc1f9019ab070eb74c
Anjaehyun <lvet0330@gmail.com>
2023-05-22 18:50:58 +0900

    #36 'Reservation' Controller 코드 Service 코드로 분리
    Controller 에 Service 코드까지 다 넣어서 개발해두었기 때문에 분리작업 진행

--- src/main/java/com/jaehyun/store/controller/user/ReservationController.java
+++ src/main/java/com/jaehyun/store/controller/user/ReservationController.java
@@ -1,9 +1,6 @@
 package com.jaehyun.store.controller.user;
 
-import com.jaehyun.store.config.JwtTokenProvider;
-import com.jaehyun.store.model.domain.Reservation;
-import com.jaehyun.store.model.repository.ReservationRepository;
-import com.jaehyun.store.model.type.EarlyCheck;
+import com.jaehyun.store.service.ReservationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
@@ -11,57 +8,26 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDateTime;
-import java.util.List;
 
 @RestController
 @RequestMapping("/reservations")
 @RequiredArgsConstructor
 public class ReservationController {
 
-    private final ReservationRepository reservationRepository;
+    private final ReservationService reservationService;
 
-    private final JwtTokenProvider jwtTokenProvider;
-
-    //예약 생성
+    // 예약 생성
     @PostMapping("/createReservation")
     public ResponseEntity<String> createReservation(
             @RequestParam("storeId") Long storeId,
             @RequestParam("reservationTime") @DateTimeFormat(pattern = "yy-MM-dd'T'HH:mm") LocalDateTime reservationTime,
             HttpServletRequest request) {
-
-        // 토큰에서 사용자 정보를 추출하여 회원의 전화번호 가져오기
-        String token = jwtTokenProvider.resolveToken(request);
-        String phoneNum = jwtTokenProvider.getUserPhoneNum(token);
-
-        // 예약 생성을 위해 필요한 값들을 사용하여 Reservation 엔티티를 생성
-        Reservation reservation = new Reservation();
-        reservation.setUserPhoneNum(phoneNum);
-        reservation.setStoreId(storeId);
-        reservation.setReservationTime(reservationTime);
-
-        // 예약 데이터를 데이터베이스에 저장
-        reservationRepository.save(reservation);
-
-        return ResponseEntity.ok("Reservation created successfully");
+        return reservationService.createReservation(storeId, reservationTime, request);
     }
 
+    // 예약 체크
     @GetMapping("/check")
     public ResponseEntity<String> checkReservation(@RequestParam("userPhoneNum") String userPhoneNum) {
-        if (userPhoneNum != null && !userPhoneNum.isEmpty()) {
-            LocalDateTime now = LocalDateTime.now();
-
-            List<Reservation> reservations = reservationRepository.findByUserPhoneNumAndReservationTimeBefore(userPhoneNum, now);
-
-            for (Reservation reservation : reservations) {
-                reservation.setComeCheck(EarlyCheck.COME);
-                reservationRepository.save(reservation);
-            }
-
-            return ResponseEntity.ok("Reservations checked successfully.");
-        } else {
-            return ResponseEntity.badRequest().body("Missing 'userPhoneNum' parameter.");
-        }
+        return reservationService.checkReservation(userPhoneNum);
     }
-
-
 }
--- src/main/java/com/jaehyun/store/service/ReservationService.java
+++ src/main/java/com/jaehyun/store/service/ReservationService.java
@@ -1,10 +1,55 @@
 package com.jaehyun.store.service;
 
+import com.jaehyun.store.config.JwtTokenProvider;
+import com.jaehyun.store.model.domain.Reservation;
+import com.jaehyun.store.model.repository.ReservationRepository;
+import com.jaehyun.store.model.type.EarlyCheck;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ReservationService {
 
+    private final ReservationRepository reservationRepository;
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public ResponseEntity<String> createReservation(Long storeId, LocalDateTime reservationTime, HttpServletRequest request) {
+        // 토큰에서 사용자 정보를 추출하여 회원의 전화번호 가져오기
+        String token = jwtTokenProvider.resolveToken(request);
+        String phoneNum = jwtTokenProvider.getUserPhoneNum(token);
+
+        // 예약 생성을 위해 필요한 값들을 사용하여 Reservation 엔티티를 생성
+        Reservation reservation = new Reservation();
+        reservation.setUserPhoneNum(phoneNum);
+        reservation.setStoreId(storeId);
+        reservation.setReservationTime(reservationTime);
+
+        // 예약 데이터를 데이터베이스에 저장
+        reservationRepository.save(reservation);
+
+        return ResponseEntity.ok("Reservation created successfully");
+    }
+
+    public ResponseEntity<String> checkReservation(String userPhoneNum) {
+        if (userPhoneNum != null && !userPhoneNum.isEmpty()) {
+            LocalDateTime now = LocalDateTime.now();
+
+            List<Reservation> reservations = reservationRepository.findByUserPhoneNumAndReservationTimeBefore(userPhoneNum, now);
+
+            for (Reservation reservation : reservations) {
+                reservation.setComeCheck(EarlyCheck.COME);
+                reservationRepository.save(reservation);
+            }
+
+            return ResponseEntity.ok("Reservations checked successfully.");
+        } else {
+            return ResponseEntity.badRequest().body("Missing 'userPhoneNum' parameter.");
+        }
+    }
 }
