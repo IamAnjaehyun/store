fd504e60aaad18d06c76c59e0cf196e7a6e8367d
Anjaehyun <lvet0330@gmail.com>
2023-05-23 12:50:36 +0900

    #37 'Reservation' Controller 코드 Service 코드로 분리
    Controller 에 Service 코드까지 다 넣어서 개발해두었기 때문에 분리작업 진행

--- src/main/java/com/jaehyun/store/controller/user/ReviewController.java
+++ src/main/java/com/jaehyun/store/controller/user/ReviewController.java
@@ -1,54 +1,23 @@
+// ReviewController.java
 package com.jaehyun.store.controller.user;
 
-import com.jaehyun.store.config.JwtTokenProvider;
-import com.jaehyun.store.model.domain.Reservation;
-import com.jaehyun.store.model.domain.Review;
 import com.jaehyun.store.model.dto.ReviewDto;
-import com.jaehyun.store.model.repository.ReservationRepository;
-import com.jaehyun.store.model.repository.ReviewRepository;
+import com.jaehyun.store.service.ReviewService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
-import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping("/review")
 @RequiredArgsConstructor
 public class ReviewController {
-    private final ReservationRepository reservationRepository;
-    private final JwtTokenProvider jwtTokenProvider;
-    private final ReviewRepository reviewRepository;
+    private final ReviewService reviewService;
 
-    //    리뷰 작성 컨트롤러
+    // 리뷰 작성 컨트롤러
     @PostMapping("/reviews")
-    public ResponseEntity<String> writeReview(@RequestBody ReviewDto reviewDto
-            ,HttpServletRequest request) {
-        // 1. 토큰을 통한 인증 및 고객 아이디 가져오기
-        String token = jwtTokenProvider.resolveToken(request);
-        String customerId = jwtTokenProvider.getUserPhoneNum(token);
-
-        // 2. 작성자 검증
-        Reservation reservation = reservationRepository.findByUserPhoneNumAndStoreId(customerId, reviewDto.getStoreId());
-        if (reservation == null) {
-            return ResponseEntity.badRequest().body("You are not authorized to write a review for this store.");
-        }
-
-        // 3. 예약 시간 확인
-        LocalDateTime currentTime = LocalDateTime.now();
-        LocalDateTime reservationTime = reservation.getReservationTime();
-        if (currentTime.isBefore(reservationTime)) {
-            return ResponseEntity.badRequest().body("You can only write a review after the reservation time has passed.");
-        }
-
-        // 4. 리뷰 작성
-        Review review = new Review();
-        review.setStoreId(reviewDto.getStoreId());
-        review.setUserId(customerId);
-        review.setReviewText(reviewDto.getReviewText());
-        reviewRepository.save(review);
-
-        return ResponseEntity.ok("Review created successfully.");
+    public ResponseEntity<String> writeReview(@RequestBody ReviewDto reviewDto, HttpServletRequest request) {
+        return reviewService.writeReview(reviewDto, request);
     }
 }
