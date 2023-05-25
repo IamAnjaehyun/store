7372a84cac24bff4b52a6365cff82a568788214d
Anjaehyun <lvet0330@gmail.com>
2023-05-25 23:38:56 +0900

    #49 상점 삭제 및 개별 조회 기능 구현
    다른 도메인에 대해서도 추가예정!

--- src/main/java/com/jaehyun/store/controller/partner/StoreController.java
+++ src/main/java/com/jaehyun/store/controller/partner/StoreController.java
@@ -1,5 +1,6 @@
 package com.jaehyun.store.controller.partner;
 
+import com.jaehyun.store.model.domain.Store;
 import com.jaehyun.store.model.dto.StoreDto;
 import com.jaehyun.store.service.StoreService;
 import io.swagger.annotations.Api;
@@ -27,8 +28,24 @@ public class StoreController {
         return ResponseEntity.ok("registerStore successfully.");
     }
 
+    //상점 삭제
+    @ApiOperation(value = "상점 삭제",notes = "파트너 권한을 가진 유저가 상점을 삭제합니다.")
+    @DeleteMapping("/delete/{storeId}")
+    public ResponseEntity<String> deleteStore(@PathVariable Long storeId, HttpServletRequest request) {
+        storeService.deleteStore(storeId, request);
+        return ResponseEntity.ok("deleteStore successfully.");
+    }
+
+    //상점 개별 조회
+    @ApiOperation(value = "상점 조회",notes = "상점의 이름을 통해 상점을 조회합니다.")
+    @GetMapping("/view/{storeName}")
+    public ResponseEntity<List<Store>> getStore(@PathVariable String storeName) {
+        List<Store> store = storeService.viewStore(storeName);
+        return ResponseEntity.ok(store);
+    }
+
     // 모든 매장 정보 확인
-    @ApiOperation(value = "상점 목록 조회",notes = "모든 매장의 목록을 확인합니다.")
+    @ApiOperation(value = "상점 전체 조회",notes = "모든 매장의 목록을 확인합니다.")
     @GetMapping("/view")
     public List<Map<String, Object>> getAllStores() {
         return storeService.getAllStores();
