b456a67d86e7fc6f29f421996ef2094e9c3ea00b
Anjaehyun <lvet0330@gmail.com>
2023-05-26 17:22:50 +0900

    #54 fix CustomUserDetailService

--- src/main/java/com/jaehyun/store/service/CustomUserDetailService.java
+++ src/main/java/com/jaehyun/store/service/CustomUserDetailService.java
@@ -1,11 +1,14 @@
 package com.jaehyun.store.service;
 
+import com.jaehyun.store.model.domain.User;
 import com.jaehyun.store.model.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -15,7 +18,10 @@ public class CustomUserDetailService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String userPhoneNum) throws UsernameNotFoundException {
-        return userRepository.findByUserPhoneNum(userPhoneNum)
-                .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
+        Optional<User> optionalUser = userRepository.findByUserPhoneNum(userPhoneNum);
+        User user = optionalUser.orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
+        return user;
     }
+
+
 }
