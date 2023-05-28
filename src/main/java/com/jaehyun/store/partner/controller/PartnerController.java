ad9b001fcc1eb3efcac7c09f666ad700bd6d672e
Anjaehyun <lvet0330@gmail.com>
2023-05-28 16:51:58 +0900

    #64 permit 과 controller 합침
    partner 가 permit에 대한 권한을 갖고있기에 나누어놓을 이유가 없다고 생각

--- src/main/java/com/jaehyun/store/partner/controller/PartnerController.java
+++ src/main/java/com/jaehyun/store/partner/controller/PartnerController.java
@@ -1,26 +1,53 @@
 package com.jaehyun.store.partner.controller;
 
 import com.jaehyun.store.partner.service.PartnerService;
+import com.jaehyun.store.type.ReservationStatus;
+import com.jaehyun.store.user.domain.entity.Reservation;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@Api(tags = "파트너 등록 컨트롤러")
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@Api(tags = "파트너 컨트롤러")
 @RestController
 @RequestMapping("/shop/v1/partner")
 @RequiredArgsConstructor
 public class PartnerController {
     private final PartnerService partnerService;
-
     @ApiOperation(value = "파트너 권한 부여",notes = "유저의 권한을 파트너로 변경시켜 상점 등록 및 여러 권한을 갖게 합니다.")
     @PostMapping("/changeRole")
     public ResponseEntity<String> changeRole(@RequestParam String userPhoneNum) {
         partnerService.changeRole(userPhoneNum);
         return ResponseEntity.ok("changeRole successfully.");
+    }
+
+    // 파트너의 전화번호를 통해 예약 목록을 조회
+    @ApiOperation(value = "예약 목록 조회", notes = "점주의 휴대폰 번호와 일치하는 정보를 가진 가게에 대한 예약 목록을 조회합니다.")
+    @GetMapping("/reservations/{userPhoneNum}")
+    public ResponseEntity<List<Reservation>> viewReservationList(@PathVariable String userPhoneNum) {
+        List<Reservation> reservations = partnerService.getReservationsByPartnerPhoneNum(userPhoneNum);
+        return ResponseEntity.ok(reservations);
+    }
+
+    // 들어온 예약에 대해 승인 또는 거절
+    @ApiOperation(value = "예약 승인 혹은 거절",notes = "예약이 들어왔을 때 점주가 예약 대기 상태를 예약 승인 혹은 거절 상태로 지정합니다.")
+    @PostMapping("/status")
+    public ResponseEntity<String> setStatus(
+            @RequestParam("reservationId") Long reservationId,
+            @RequestParam("status") ReservationStatus status,
+            HttpServletRequest request) {
+
+        // 예약 상태 업데이트
+        boolean updated = partnerService.updateReservationStatus(reservationId, request, status);
+        if (updated) {
+            return ResponseEntity.ok("Reservation status updated successfully.");
+        } else {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("You are not authorized to update the reservation status.");
+        }
     }
 }
--- src/main/java/com/jaehyun/store/partner/service/PartnerService.java
+++ src/main/java/com/jaehyun/store/partner/service/PartnerService.java
@@ -1,16 +1,29 @@
 package com.jaehyun.store.partner.service;
 
+import com.jaehyun.store.global.config.JwtTokenProvider;
+import com.jaehyun.store.partner.domain.entity.Store;
+import com.jaehyun.store.partner.domain.repository.StoreRepository;
+import com.jaehyun.store.type.ReservationStatus;
+import com.jaehyun.store.user.domain.entity.Reservation;
 import com.jaehyun.store.user.domain.entity.User;
+import com.jaehyun.store.user.domain.repository.ReservationRepository;
 import com.jaehyun.store.user.domain.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class PartnerService {
     private final UserRepository userRepository;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final ReservationRepository reservationRepository;
+    private final StoreRepository storeRepository;
 
     //권한 변경
     public void changeRole(String userPhoneNum) {
@@ -20,5 +33,45 @@ public class PartnerService {
         member.setRoles(Collections.singletonList("ADMIN")); // roles를 "ADMIN"으로 변경
 
         userRepository.save(member);
+    }
+
+    // 파트너의 전화번호를 통해 예약 목록을 조회
+    public List<Reservation> getReservationsByPartnerPhoneNum(String partnerPhoneNum) {
+        // 상점 주인의 userPhoneNum을 통해 상점들을 조회
+        List<Store> stores = storeRepository.findByUserPhoneNum(partnerPhoneNum);
+
+        // 상점들의 storeId 목록을 추출
+        List<Long> storeIds = stores.stream()
+                .map(Store::getStoreId)
+                .collect(Collectors.toList());
+
+        // 상점들의 storeId 목록을 통해 예약 목록을 조회
+        return reservationRepository.findByStoreIdIn(storeIds);
+    }
+
+    // 토큰에서 전화번호 추출
+    public String extractUserPhoneNumFromToken(HttpServletRequest request) {
+        String token = jwtTokenProvider.resolveToken(request);
+        return jwtTokenProvider.getUserPhoneNum(token);
+    }
+
+    // 예약 상태 업데이트
+    public boolean updateReservationStatus(Long reservationId, HttpServletRequest request, ReservationStatus status) {
+        // 토큰을 통해 상점 주인의 userPhoneNum 가져오기
+        String token = jwtTokenProvider.resolveToken(request);
+        String storeOwnerUserPhoneNum = jwtTokenProvider.getUserPhoneNum(token);
+
+        Optional<Reservation> optionalReservation = reservationRepository.findById(reservationId);
+        if (optionalReservation.isPresent()) {
+            Reservation reservation = optionalReservation.get();
+            Optional<Store> optionalStore = storeRepository.findByStoreId(reservation.getStoreId());
+            if (optionalStore.isPresent() && optionalStore.get().getUserPhoneNum().equals(storeOwnerUserPhoneNum)) {
+                // 예약 상태 업데이트 로직
+                reservation.setStatus(status);
+                reservationRepository.save(reservation);
+                return true;
+            }
+        }
+        return false;
     }
 }
