23b0ec86673b4ede7d175e6db35b3c8a65d093fc
Anjaehyun <lvet0330@gmail.com>
2023-05-29 17:37:47 +0900

    #72 날짜 기준으로 예약 조회 기능 구현
    모든 날짜에 대한 예약 조회와, 현재 날짜 이후의 대한 예약을 조회할 수 있게 기능 구현
    모든 예약 조회 기능은 시간이 빠른 순으로 정렬하였음.

--- src/main/java/com/jaehyun/store/partner/controller/PartnerController.java
+++ src/main/java/com/jaehyun/store/partner/controller/PartnerController.java
@@ -19,23 +19,29 @@ import java.util.List;
 @RequiredArgsConstructor
 public class PartnerController {
     private final PartnerService partnerService;
-    @ApiOperation(value = "파트너 권한 부여",notes = "유저의 권한을 파트너로 변경시켜 상점 등록 및 여러 권한을 갖게 합니다.")
+
+    @ApiOperation(value = "파트너 권한 부여", notes = "유저의 권한을 파트너로 변경시켜 상점 등록 및 여러 권한을 갖게 합니다.")
     @PostMapping("/changeRole")
     public ResponseEntity<String> changeRole(@RequestParam String userPhoneNum) {
         partnerService.changeRole(userPhoneNum);
         return ResponseEntity.ok("changeRole successfully.");
     }
 
-    // 파트너의 전화번호를 통해 예약 목록을 조회
-    @ApiOperation(value = "예약 목록 조회", notes = "점주의 휴대폰 번호와 일치하는 정보를 가진 가게에 대한 예약 목록을 조회합니다.")
-    @GetMapping("/reservations/{userPhoneNum}")
-    public ResponseEntity<List<Reservation>> viewReservationList(@PathVariable String userPhoneNum) {
-        List<Reservation> reservations = partnerService.getReservationsByPartnerPhoneNum(userPhoneNum);
+    @ApiOperation(value = "전체 예약 목록 조회", notes = "점주의 휴대폰 번호와 일치하는 정보를 가진 가게에 대한 전체 예약 목록을 조회합니다.")
+    @GetMapping("/reservations/all")
+    public ResponseEntity<List<Reservation>> viewAllReservationList(HttpServletRequest request) {
+        List<Reservation> reservations = partnerService.getReservationsByPartnerPhoneNum(request);
         return ResponseEntity.ok(reservations);
     }
 
-    // 들어온 예약에 대해 승인 또는 거절
-    @ApiOperation(value = "예약 승인 혹은 거절",notes = "예약이 들어왔을 때 점주가 예약 대기 상태를 예약 승인 혹은 거절 상태로 지정합니다.")
+    @ApiOperation(value = "현재시간 이후의 예약 목록 조회", notes = "점주의 휴대폰 번호와 일치하는 정보를 가진 가게에 대한 현재시간 이후의 예약 목록을 조회합니다.")
+    @GetMapping("/reservations/after")
+    public ResponseEntity<List<Reservation>> viewReservationList(HttpServletRequest request) {
+        List<Reservation> reservations = partnerService.getReservationsByPartnerPhoneNumAfterNow(request);
+        return ResponseEntity.ok(reservations);
+    }
+
+    @ApiOperation(value = "예약 승인 혹은 거절", notes = "예약이 들어왔을 때 점주가 예약 대기 상태를 예약 승인 혹은 거절 상태로 지정합니다.")
     @PostMapping("/status")
     public ResponseEntity<String> setStatus(
             @RequestParam("reservationId") Long reservationId,
--- src/main/java/com/jaehyun/store/partner/service/PartnerService.java
+++ src/main/java/com/jaehyun/store/partner/service/PartnerService.java
@@ -12,6 +12,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
@@ -35,19 +36,49 @@ public class PartnerService {
         userRepository.save(member);
     }
 
-    // 파트너의 전화번호를 통해 예약 목록을 조회
-    public List<Reservation> getReservationsByPartnerPhoneNum(String partnerPhoneNum) {
+    // 파트너의 전화번호를 통해 전체 예약 목록을 조회
+    public List<Reservation> getReservationsByPartnerPhoneNum(HttpServletRequest request) {
+        String token = jwtTokenProvider.resolveToken(request);
+        String storeOwnerUserPhoneNum = jwtTokenProvider.getUserPhoneNum(token);
         // 상점 주인의 userPhoneNum을 통해 상점들을 조회
-        List<Store> stores = storeRepository.findByUserPhoneNum(partnerPhoneNum);
+        List<Store> stores = storeRepository.findByUserPhoneNum(storeOwnerUserPhoneNum);
 
         // 상점들의 storeId 목록을 추출
         List<Long> storeIds = stores.stream()
                 .map(Store::getStoreId)
                 .collect(Collectors.toList());
 
-        // 상점들의 storeId 목록을 통해 예약 목록을 조회
-        return reservationRepository.findByStoreIdIn(storeIds);
+        // 상점들의 storeId 목록을 통해 예약 목록을 시간 빠른 순서로
+        return reservationRepository.findByStoreIdInOrderByReservationTimeAsc(storeIds);
     }
+
+    // 파트너의 전화번호를 통해 지금시간 이후의 예약 목록을 조회
+    public List<Reservation> getReservationsByPartnerPhoneNumAfterNow(HttpServletRequest request) {
+        String token = jwtTokenProvider.resolveToken(request);
+        String storeOwnerUserPhoneNum = jwtTokenProvider.getUserPhoneNum(token);
+
+        // 상점 주인의 userPhoneNum을 통해 상점들을 조회
+        List<Store> stores = storeRepository.findByUserPhoneNum(storeOwnerUserPhoneNum);
+
+        // 상점들의 storeId 목록을 추출
+        List<Long> storeIds = stores.stream()
+                .map(Store::getStoreId)
+                .collect(Collectors.toList());
+
+        // 현재 날짜와 시간을 가져옴
+        LocalDateTime currentDateTime = LocalDateTime.now();
+
+        // 상점들의 storeId 목록을 통해 예약 목록을 조회
+        List<Reservation> reservations = reservationRepository.findByStoreIdIn(storeIds);
+
+        // 현재 날짜와 비교하여 이미 지난 예약을 필터링
+        List<Reservation> filteredReservations = reservations.stream()
+                .filter(reservation -> reservation.getReservationTime().isAfter(currentDateTime))
+                .collect(Collectors.toList());
+
+        return filteredReservations;
+    }
+
 
     // 토큰에서 전화번호 추출
     public String extractUserPhoneNumFromToken(HttpServletRequest request) {
